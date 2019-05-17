7d31bc5d5d28ea44425347f5437eacea7c08513f
Teresa Cho <tcho@cca.edu>
2019-05-17 11:06:49 -0700

    made models and connected it to view

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Cat
 
 # Create your views here.
 def index(request): 
+    cats = Cat.objects.all()
     return render(request, 'index.html', {'cats': cats})
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
